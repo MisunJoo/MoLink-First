f2afab5d68819711ce8e47db28f774a69ef4540a
MisunJoo <0987someday@naver.com>
2019-07-12 12:23:17 +0900

    [ADD] 관심사 Category 내려주기 API

--- src/main/java/com/mashup/molinkfirst/category/CategoryController.java
+++ src/main/java/com/mashup/molinkfirst/category/CategoryController.java
@@ -3,16 +3,27 @@ package com.mashup.molinkfirst.category;
 import com.mashup.molinkfirst.category.dto.ResCategories;
 import com.mashup.molinkfirst.model.ApiResponseModel;
 import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/categories")
 public class CategoryController {
+  private CategoryService categoryService;
 
-//
-//  public ApiResponseModel<List<ResCategories>> getCategories(){
-//
-//    return
-//  }
+  public CategoryController(CategoryService categoryService) {
+    this.categoryService = categoryService;
+  }
+
+  @GetMapping("")
+  public ApiResponseModel<List<ResCategories>> getCategories(){
+    ApiResponseModel<List<ResCategories>> response = new ApiResponseModel<>();
+    response.setStatusCode(HttpStatus.OK.value());
+    response.setMessage(HttpStatus.OK.toString());
+    response.setData(categoryService.getAllCategories());
+
+    return response;
+  }
 }
--- src/main/java/com/mashup/molinkfirst/category/CategoryService.java
+++ src/main/java/com/mashup/molinkfirst/category/CategoryService.java
@@ -1,8 +1,30 @@
 package com.mashup.molinkfirst.category;
 
+import com.mashup.molinkfirst.category.dto.ResCategories;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CategoryService {
+  private CategoryRepository categoryRepository;
 
+  public CategoryService(CategoryRepository categoryRepository) {
+    this.categoryRepository = categoryRepository;
+  }
+
+  public List<ResCategories> getAllCategories(){
+    List<Category> categories = categoryRepository.findAll();
+    List<ResCategories> resCategories = new ArrayList<>();
+    int categoryNum = categories.size();
+
+    for (int i = 0; i < categoryNum; i++) {
+      ResCategories resCategory = new ResCategories();
+      resCategory.setId(categories.get(i).getId());
+      resCategory.setName(categories.get(i).getName());
+      resCategory.setImgUrl(categories.get(i).getImgUrl());
+      resCategories.add(resCategory);
+    }
+    return resCategories;
+  }
 }
